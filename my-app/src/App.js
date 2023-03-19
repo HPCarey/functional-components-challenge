e77fb969ab0e8d87ae2ac8e07c5e2948045f3433
HPCarey <hannahpcarey@gmail.com>
2023-03-19 16:41:18 +0000

    convert ClassApi to functional componenet with hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,13 +6,15 @@ import NavBarChild from './components/NavBarChild';
 import Contents from './components/Contents';
 import ContentHooks from './components/ContentHooks';
 import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <Sidebar />
       <NavBarForm />
-      <ContentAPI />
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
       {/* <Contents /> */}
       {/* <ContentHooks /> */}
       {/* <NavBarSimple /> */}
